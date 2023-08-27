87a087d329593139f87f810b1158288170f2631f
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-08-27 17:16:55 -0400

    added JS to project

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript', 'Command Line', 'Random Notes'];
+
+for (var x=0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else if (topic === 'Command Line') {
+  console.log("Let's study Command Line!");
+} else if (topic === 'Random Notes') {
+  console.log("Let's study Random Notes!");
+}
+else {
+  console.log('Please try again!');
+}
+*/
