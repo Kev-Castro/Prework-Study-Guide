d01eb480577eba589249ed9994f3dcae59cc1905
Kevin Castro <143019683+Kev-Castro@users.noreply.github.com>
2023-08-27 19:48:07 -0400

    Updated project README file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript', 'Command Line', 'Random Notes'];
-<<<<<<< HEAD
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
@@ -20,30 +19,10 @@ if (randomTopic === 'HTML') {
 } else if (randomTopic === 'Command Line') {
   console.log("Let's study Command Line!");
 } else if (randomTopic === 'Random Notes') {
-=======
-
-for (var x=0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
-
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else if (topic === 'Command Line') {
-  console.log("Let's study Command Line!");
-} else if (topic === 'Random Notes') {
->>>>>>> 87a087d329593139f87f810b1158288170f2631f
   console.log("Let's study Random Notes!");
 }
 else {
   console.log('Please try again!');
-<<<<<<< HEAD
  }
 }
 
@@ -52,7 +31,3 @@ console.log('Here are the topics we learned through Prework:');
 listTopics()
 console.log('Which topic should we study first?');
 selectTopic();
-=======
-}
-*/
->>>>>>> 87a087d329593139f87f810b1158288170f2631f
